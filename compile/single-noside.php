d5f234a907dcd09685e2e950647fd8cb6f4b5aac
zitonglu <910109610@qq.com>
2016-09-06 17:08:35 +0800

    single-noside.php

---
+++ compile/single-noside.php
@@ -0,0 +1,45 @@
+
+<?php  include $this->GetTemplate('header');  ?>
+<body id="single-noside">
+<!-- 导航 -->
+<div class="container">
+	<?php  include $this->GetTemplate('nav');  ?>
+	<div class="head row">
+		<div class="col-sm-4 hidden-xs">
+			<img src="<?php  echo $host;  ?>zb_users/theme/<?php  echo $theme;  ?>/include/logo.png" alt="<?php  echo $name;  ?>的网站LOGO">
+		</div>
+		<div class="col-sm-5 col-sm-offset-3 col-md-4 col-md-offset-4 search-box hidden-xs">
+			<form class="navbar-form navbar-left" role="search" action="<?php  echo $host;  ?>zb_system/cmd.php?act=search" name="search" method="post">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="输入搜索内容" name="q">
+				</div>
+				<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span></button>
+			</form>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row single-body single-box">
+		<!-- <div class="col-md-9 col-md-offset-125"> -->
+<?php if ($article->Type==ZC_POST_TYPE_ARTICLE) { ?>
+<?php  include $this->GetTemplate('post-single');  ?>
+<?php }else{  ?>
+<?php  include $this->GetTemplate('post-page');  ?>
+<?php } ?>
+		<!-- </div> -->
+	</div>
+</div>
+<div class="container-fluid bottom">
+	<div class="container"><div class="row">
+		<div class="col-sm-7 bottom-div">
+			<p>将这个网站<a class="bottom-a" href="#" role="button" data-toggle="modal" data-target="#myshare">分享</a>给您的朋友吧！</p>
+		</div>
+		<div class="btn-group col-sm-5 bottom-div">
+			<a class="btn btn-default" href="<?php  echo $article->Prev->Url;  ?>" title="<?php  echo $article->Prev->Title;  ?>" role="button"><span class="glyphicon glyphicon-chevron-left"></span>&nbsp;上一篇</a>
+			<a class="btn btn-default" href="#" role="button" data-toggle="modal" data-target="#myshare"><span class="glyphicon glyphicon-qrcode"></span>&nbsp;微信分享</a>
+			<a class="btn btn-default" href="<?php  echo $article->Next->Url;  ?>" title="<?php  echo $article->Next->Title;  ?>" role="button">下一篇&nbsp;<span class="glyphicon glyphicon-chevron-right"></span></a>
+		</div>
+	</div></div>
+</div>
+<?php  include $this->GetTemplate('footer');  ?>
--- template/post-page.php
+++ template/post-page.php
@@ -11,38 +11,6 @@
   {$article.Content}
 </div>
 
-<div class="more-text">
-{php}
-$stime = time();
-$ytime = 91*24*60*60;
-$ztime = $stime-$ytime;
-$order = array('log_ViewNums'=>'DESC');
-$where = array(array('=','log_Status','0'),array('>','log_PostTime',$ztime));
-$RMarray = $zbp->GetArticleList(array('*'),$where,$order,array(6),'');
-{/php}
-{foreach $RMarray as $hotlist}
-{php}
-SF_img1::getPics($hotlist,355,230,4);
-$src=SF_img1::getPicUrlBy($hotlist->Metas->paipk1_teSeTuPian,355,230,4);
-$randABC=rand(1,20);
-{/php}
-  <div class="col-md-4 col-sm-6">
-{if $hotlist.Metas.paipk1_teSeTuPian!=""}
-  <div class="more-text-box" style="background-image:url({$src})">
-{elseif $hotlist->sf_img_count>0}
-  <div class="more-text-box" style="background-image:url({$hotlist.sf_img[0]})">
-{else}
-  <div class="more-text-box" style="background-image:url({$host}zb_users/theme/{$theme}/images/rand/{$randABC}.jpg)">
-{/if}
-      <p class="BMT-title">
-        <a href="{$hotlist.Url}" title="{$hotlist.Category.Name}">{$hotlist.Category.Name}</a><br><br>近三个月被浏览 {$hotlist.ViewNums}次</p><p class="more-text-title"><a href="{$hotlist.Url}" title="{$hotlist.Title}">{$hotlist.Title}</a>
-      </p>
-    </div>
-  </div>
-{/foreach}
-  <div class="clearfix"></div>
-</div>
-
 {if !$article.IsLock}
 {template:comments}		
 {/if}
