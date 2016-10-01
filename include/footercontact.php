1fce2bcde5d7b0637299fe91f2c8b2568b9ac2e5
紫铜炉 <910109610@qq.com>
2016-10-01 10:10:04 +0800

    change PC

--- include/footercontact.php
+++ include/footercontact.php
@@ -1,2 +1,2 @@
-<li><a href="http://www.paipk.com/?id=67" title="BUG提交" target="_blank">问题提交</a></li>
-<li><a href="http://www.paipk.com/?id=340" title="留言" target="_blank">留言页面</a></li>
+<li><a href="http://www.paipk.com/67.html" title="BUG提交" target="_blank">问题提交</a></li>
+<li><a href="http://www.paipk.com/340.html" title="留言" target="_blank">留言页面</a></li>
