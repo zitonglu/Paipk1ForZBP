98d383a49356cba78d3f75f6a514d48ef268020f
zitonglu <910109610@qq.com>
2016-11-17 14:15:07 +0800

    delete color.css OH my head

--- template/istop.php
+++ template/istop.php
@@ -1,7 +1,7 @@
 <?php echo'<meta charset="UTF-8"><div style="text-align:center;padding:60px 0;font-size:16px;">
 		<h2 style="font-size:60px;margin-bottom:32px;color:f00;">主题由<a href="http://www.paipk.com">紫铜炉</a>设计制作</h2>
 </div>';die();?>
-<div id="carousel-example-generic" class="post-box carousel slide" data-ride="carousel">
+<div id="carousel-example-generic" class="carousel slide topbox" data-ride="carousel">
 	<div class="carousel-inner" role="listbox">
 		{php}
 		if(is_file($zbp->path.'zb_users/theme/paipk1/plugin/out.html')){
