13e9c17fd1c39dd24fc50442f9b798f0fe03614e
zitonglu <910109610@qq.com>
2016-12-10 08:14:19 +0800

    clear ID=top

--- template/search.php
+++ template/search.php
@@ -2,7 +2,7 @@
 <h2 style="font-size:60px;margin-bottom:32px;color:f00;">主题由<a href="http://www.paipk.com">紫铜炉</a>设计制作</h2>
 </div>';die();?>
 {template:header}
-<body id="top" class="single single-search">
+<body class="single single-search">
 {template:nav}<!-- nav end -->
 <article class="container article">
 	{if $article.Type==ZC_POST_TYPE_ARTICLE}
