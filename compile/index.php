7892ac6f4aeb3e3b80b30b06fa96b4a26b003126
zitonglu <9109610@qq.com>
2018-10-29 14:12:18 +0800

    del compile

--- compile/index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if ($zbp->Config('paipk1')->indexTheme!="") {
-	include $this->GetTemplate('index-'.$zbp->Config('paipk1')->indexTheme);
-}else{
-	include $this->GetTemplate('index-two');
-}
- ?>
